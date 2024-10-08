725337c4874c66d3cf47d2694bba99206ce2d8f2
muhammadrayhanwidayat <murayat99@gmail.com>
2024-10-08 10:38:46 +0700

    first commit

---
+++ latihan1.py
@@ -0,0 +1,16 @@
+# Print your name to the console.
+#print nama rayhan
+print("rayhan")
+
+#Print the sentence "I am learning Python basics." on a new line.
+#print saya belajar python
+print("I am learning Python basics.")
+
+#Write a program that asks the user for their name and then greets them with "Hello, [Name]!".
+#menyimpan input pengguna di name trus print namanya
+name = input("What is your name? ")
+print("Hello, " + name + "!")
+
+#Program meminta input untuk usia trus cetak
+age = input("How old are you? ")
+print(age)
